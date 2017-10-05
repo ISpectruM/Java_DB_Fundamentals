780ba0ad11194bc5583372b035a492d9cd60ffd2
ISpectruM <ispectrumcode@gmail.com>
2017-10-05 22:55:22 +0300

    Build-in Functions Exercises: 15.Show All Games with Duration and Part of the Day.

---
+++ A_Databases_Basics_MySQL/C_Built-in Functions/Exercises/p15_if_else.sql
@@ -0,0 +1,7 @@
+SELECT g.name,
+IF(HOUR(g.`start`)>=0 AND HOUR(g.`start`)<12,'Morning',
+	IF(HOUR(g.`start`)>=12 AND HOUR(g.`start`) < 18,'Afternoon','Evening')) AS 'Part of the Day',
+IF(g.duration <= 3,'Extra Short',
+	IF(g.duration BETWEEN 4 AND 6,'Short',
+		IF(g.duration BETWEEN 7 AND 10, 'Long','Extra Long'))) AS 'Duration'
+FROM games AS g;
