c41042dd60d9a4763cba7bb4c4d0e0ba4a65e513
ISpectruM <ispectrumcode@gmail.com>
2017-10-18 12:26:48 +0300

    Subqueries and Joins exercises: 13.	Count Mountain Ranges.

---
+++ A_Databases_Basics_MySQL/F_Subqueries_Joins/Exercises/p13_join_group_count.sql
@@ -0,0 +1,7 @@
+SELECT c.country_code,COUNT(m.mountain_range) AS mountain_range
+FROM countries AS c
+JOIN mountains_countries AS mc ON mc.country_code = c.country_code
+JOIN mountains AS m ON m.id = mc.mountain_id
+WHERE c.country_code = 'BG' OR c.country_code = 'RU' OR c.country_code = 'US'
+GROUP BY c.country_code
+ORDER BY mountain_range DESC;
