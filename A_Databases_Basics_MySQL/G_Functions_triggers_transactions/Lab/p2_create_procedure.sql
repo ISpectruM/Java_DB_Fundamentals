eb852d9278dc1d4a76c9e77205cb6bff5a03f3ba
ISpectruM <ispectrumcode@gmail.com>
2017-10-20 11:27:43 +0300

    Subqueries and Joins exercises: 1.	Employees with Salary Above 35000.

--- A_Databases_Basics_MySQL/G_Functions_triggers_transactions/Lab/p2_create_procedure.sql
+++ A_Databases_Basics_MySQL/G_Functions_triggers_transactions/Lab/p2_create_procedure.sql
@@ -1,7 +1,8 @@
+DELIMITER $$
 CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(20))
 BEGIN
 	UPDATE employees AS e
 	JOIN departments AS d ON d.department_id = e.department_id
 	SET e.salary = e.salary*1.05
 	WHERE d.name = department_name;
-END
+END $$
